5a615bc8861ec7aac145890f3c3f4933a4641c66
Karan's Study <study@Karans-MacBook-Pro.local>
2023-08-05 12:27:17 -0400

    add monolith to microservice project

---
+++ Project: Refactor Monolith to Microservices and Deploy /set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacityproject
+export POSTGRES_PASSWORD=udacityproject
+export POSTGRES_HOST=udacityproject.cvgslqza2a60.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udacity-udagram-api
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=karangajjar
+export URL=http://localhost:8100
